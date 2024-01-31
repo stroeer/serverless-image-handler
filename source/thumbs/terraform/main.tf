e52430d478a02f03298e581522c3cb349009cd45
Moritz Zimmer <moritz.zimmer+github@posteo.de>
2024-01-31 17:05:58 +0100

    thumbs stack

--- source/thumbs/terraform/main.tf
+++ source/thumbs/terraform/main.tf
@@ -7,7 +7,7 @@ locals {
 
 module "lambda" {
   source  = "registry.terraform.io/moritzzimmer/lambda/aws"
-  version = "7.2.0"
+  version = "7.5.0"
 
   architectures = ["arm64"]
   layers = [
@@ -80,12 +80,13 @@ resource "aws_s3_object" "this" {
 
 module "deployment" {
   source  = "registry.terraform.io/moritzzimmer/lambda/aws//modules/deployment"
-  version = "7.2.0"
+  version = "7.5.0"
 
   alias_name                                  = aws_lambda_alias.this.name
   codebuild_cloudwatch_logs_retention_in_days = 7
   codestar_notifications_target_arn           = data.aws_sns_topic.notifications.arn
   codepipeline_artifact_store_bucket          = data.aws_s3_bucket.pipeline_artifacts.bucket
+  codepipeline_type                           = "V2"
   s3_bucket                                   = data.aws_s3_bucket.ci.bucket
   s3_key                                      = local.s3_key
   function_name                               = local.function_name
